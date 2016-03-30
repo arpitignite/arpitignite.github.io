6fa276e68d52f81286b9e823fd8b7fcc7a73eba6
Arpit Gandhi <arpit.gandhi@ignitesol.com>
2016-03-30 11:30:27 +0530

    Anand

--- acc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$text = "accX".$_GET['accX'].", accY".$_GET['accY'].", accZ".$_GET['accZ'].PHP_EOL;
-file_put_contents('values.csv', $text, FILE_APPEND);
-?>
--- acc2.php
+++
@@ -1,4 +0,0 @@
-<?php
-$text = "accX".$_GET['accX'].", accY".$_GET['accY'].", accZ".$_GET['accZ'].PHP_EOL;
-file_put_contents('values_not_considered.csv', $text, FILE_APPEND);
-?>
